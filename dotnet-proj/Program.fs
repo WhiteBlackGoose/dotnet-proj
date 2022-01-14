b918a7967c99bacaee68bed865773b21572d52c5
WhiteBlackGoose <wbg@angouri.org>
2022-01-14 10:50:21 +0300

    Creating is now possible

--- dotnet-proj/Program.fs
+++ dotnet-proj/Program.fs
@@ -1,4 +1,35 @@
-﻿
+﻿open CommandLine
+open System.IO
 
 // For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+type Options = {
+    [<CommandLine.Option('o', "object", Required = false, HelpText = "Path to Directory.Build.* or csproj etc.")>]
+    project : string;
+
+    [<CommandLine.Option('c', "create", Default = false, Required = false, HelpText = "Create a file with the given name")>]
+    create : bool;
+
+    [<CommandLine.Option('p', "property", Required = false, HelpText = "Path to property")>]
+    proeprtyValue : string;
+}
+    
+
+let getActualProject project =
+    // TODO: make auto-detect
+    project
+
+let createDirectoryBuild file =
+    let content = "<Project>\n\n</Project>"
+    File.WriteAllText(file, content)
+    let fullFile = Path.GetFullPath file
+    printfn $"Written to {fullFile}"
+
+CommandLine.Parser.Default.ParseArguments<Options>(System.Environment.GetCommandLineArgs())
+    .WithParsed<Options>(fun o ->
+        let prj = getActualProject o.project
+
+        if o.create then
+            createDirectoryBuild prj
+
+    ) |> ignore
+
