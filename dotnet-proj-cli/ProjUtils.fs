52e301251debfaf6088c084875785fff3c890965
WhiteBlackGoose <wbg@angouri.org>
2022-01-18 15:01:55 +0300

    Fixes

--- dotnet-proj-cli/ProjUtils.fs
+++ dotnet-proj-cli/ProjUtils.fs
@@ -24,7 +24,7 @@ let (|EndsWith|_|) (s : string) (input : string) =
 
 let createDirectoryBuild (file : string) =
     let content =
-        match file with
+        match Path.GetFileName file with
         | StartsWith "Directory.Build." ->
             "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<Project>\n\n</Project>" |> Some
         | EndsWith "proj" ->
@@ -32,9 +32,11 @@ let createDirectoryBuild (file : string) =
         | _ -> None
     match content with
     | Some content ->
+        let fullPath = Path.GetFullPath file
+        let dir = Path.GetDirectoryName fullPath
+        Directory.CreateDirectory dir |> ignore
         File.WriteAllText(file, content)
-        let fullFile = Path.GetFullPath file
-        printfn $"Created in {fullFile}"
+        printfn $"Created in {fullPath}"
         0
     | None ->
         printfn $"Cannot detect the right format, create *proj or Directory.Build.* files"
@@ -49,8 +51,10 @@ let addPropertyOrItem (typeName : string) (file : string) property value attribu
 
     let projects = doc.Elements "Project"
     
-    assertFact (Seq.length projects = 1) "Should be one project element!"
-    let projectElement = projects |> Seq.head
+    let projectElement = 
+        projects
+        |> Seq.tryExactlyOne
+        |> Option.defaultWith (fun _ -> failwith "Should be one project element!")
     
     let group =
         match projectElement.Elements () |> Seq.tryLast with
